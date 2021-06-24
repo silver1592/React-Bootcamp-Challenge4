f4a7c3c1c9869607f3e10bd55ff1993a04f32eaf
Fernando Alvarez Flores <feral1592@gmail.com>
2021-06-24 00:38:11 -0500

    Display pokemon name

--- src/PokeNameValidator.js
+++ src/PokeNameValidator.js
@@ -5,13 +5,25 @@ export default function PokeNameValidator() {
   /**
    * Component logic goes here
    */
+  const [selectedPokemon, setSelectedPokemon] = useState(pokemons[0]);
+
+  function RandomPokemon(currentPokemon) {
+    let nextPokemon = {};
+    do {
+      let randomNumber = Math.floor(Math.random() * pokemons.length);
+      nextPokemon = pokemons[randomNumber];
+      console.log(nextPokemon);
+    } while (nextPokemon === currentPokemon);
+
+    return nextPokemon;
+  }
 
   return (
     <div className="app">
       <h2>Mini Challenge 4: Poke Name Validator</h2>
       <p>
         Pokémon name:&nbsp;
-        <strong>{/** random pokemon name should appear here */}</strong>
+        <strong>{selectedPokemon}</strong>
       </p>
       <p className="secondary">Type the Pokémon name correctly:</p>
       <input type="text" placeholder="Enter the Pokémon name" />
